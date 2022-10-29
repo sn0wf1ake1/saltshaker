54a0206a8b2748603efbf39bdc78fdb540484019
Anders Raben Hansen <12665561+sn0wf1ake1@users.noreply.github.com>
2022-10-29 23:38:59 +0200

    Update saltshaker.ps1

--- saltshaker.ps1
+++ saltshaker.ps1
@@ -3,8 +3,7 @@ Clear-Host
 <# Password start #>
 $password = "Password"
 $password = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($($password)))
-$password_salted = ''
-$password_salted_temp = ''
+$password_salted = $password_salted_temp = ''
 
 $x = ([long][char]$password.Substring(0,1) / [math]::E).ToString().Substring(3)
 $y = ''
@@ -14,7 +13,7 @@ for($i = 0; $i -lt $password.Length; $i++) {
     $y += ($x.Substring($x.Length - 6,6) / [math]::E).ToString()
 }
 
-$password_salted_temp = $x.Replace(',','').Replace('.','')
+$password_salted_temp = $x -replace "[^0-9]"
 
 for($i = 0; $i -le $password_salted_temp.Length - ($password_salted_temp.Length % 3) - 1; $i += 3) {
     $password_salted += [System.Convert]::ToString($password_salted_temp.Substring($i,3) % 255,2).PadLeft(8,'0')
@@ -28,7 +27,7 @@ function saltshaker() {
         [Parameter(Mandatory = $true)] [byte]$debugging
     )
 
-    if($debugging -eq 1){Write-Host ('Password salted: ' + ($password_salted).Substring(0,128) + '...')} # Salt is way too long to display
+    if($debugging -eq 1){Write-Host ('Password salted:  ' + ($password_salted).Substring(0,128) + '...')} # Salt is way too long to display
 
     <# UTF-8 encode into 16 byte blocks start #>
     [string]$blocks_encoded = ''
@@ -44,7 +43,7 @@ function saltshaker() {
         $blocks_encoded += $utf
     }
 
-    if($debugging -eq 1){Write-Host ('UTF-8 encoded:   ' + $blocks_encoded)}
+    if($debugging -eq 1){Write-Host ('UTF-8 encoded:    ' + $blocks_encoded)}
     <# UTF-8 encode into 16 byte blocks end #>
 
     <# Encrypt start #>
@@ -54,7 +53,7 @@ function saltshaker() {
         $utf_binary += [System.Convert]::ToString($block,2).PadLeft(8,'0')
     }
 
-    if($debugging -eq 1){Write-Host ('UTF-8 binary:    ' + $utf_binary -join ' ')}
+    if($debugging -eq 1){Write-Host ('UTF-8 binary:     ' + $utf_binary -join ' ')}
 
     for($i = 0; $i -lt $rotations; $i++) {
         for($j = 0; $j -lt 128; $j++) {
@@ -63,8 +62,8 @@ function saltshaker() {
     }
 
     $utf_binary = $utf_binary.Substring($utf_binary.Length - 128,128)
-    <# Encrypt end #>
 
+    if($debugging -eq 1){Write-Host ('Encrypted binary: ' + $utf_binary)}
     <# Decrypt start #>
     for($i = 0; $i -lt $rotations; $i++) {
         for($j = 0; $j -lt 128; $j++) {
@@ -79,7 +78,7 @@ function saltshaker() {
         $blocks_encoded += [char][convert]::ToInt32($utf_binary.Substring($i * 8,8),2)
     }
 
-    if($debugging -eq 1){Write-Host ('UTF-8 decoded:   ' + $blocks_encoded)}
+    if($debugging -eq 1){Write-Host ('UTF-8 decoded:    ' + $blocks_encoded)}
     <# Decrypt end #>
 
     <# UTF-8 decode start #>
@@ -112,12 +111,12 @@ for($i = 0; $i -lt (4 - ($data.Length + 1) % 4) % 4; $i++) {
 $data = ((4 - ($data.Length + 1) % 4) % 4).ToString() + $data + $data_padding # First byte counts how many padded characters has been added to the final block
 
 for($i = 0; $i -lt $data.Length / 4; $i++) {
-    $blocks_decoded_array += saltshaker $data.Substring($i * 4,4) (($i % 4) + 1) 1
+    $blocks_decoded_array += saltshaker $data.Substring($i * 4,4) ($i / 4 % 5) 1
 }
 
 for($i = 0; $i -lt $blocks_decoded_array.Count; $i++) {
     $blocks_decoded_string += $blocks_decoded_array[$i]
 }
 
-Write-Host ('Decrypted:       ' + $blocks_decoded_string.Substring(1, $blocks_decoded_string.Length - ([int]$blocks_decoded_string.Substring(0,1) + 1)))
+Write-Host ('Decrypted:        ' + $blocks_decoded_string.Substring(1, $blocks_decoded_string.Length - ([int]$blocks_decoded_string.Substring(0,1) + 1)))
 <# String divided into 4 character blocks to be encrypted end #>
